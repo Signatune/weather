7d910c38b95f56bf7b0ddb012bf5c0b6a512c644
Mike <michaelambrosino93@gmail.com>
2023-10-24 11:38:47 -0400

    initial setup

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,8 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: "New Project",
+      title: "Weather",
+      template: "src/test.html",
     }),
   ],
   module: {
